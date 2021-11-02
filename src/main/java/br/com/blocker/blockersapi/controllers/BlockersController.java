1888767b6e38f4ae22614b8d267e4b19f9643441
Filipe Munhoz <filipemunhoz@gmail.com>
2021-11-02 11:36:47 -0300

    Added Performance JMX

--- src/main/java/br/com/blocker/blockersapi/controllers/BlockersController.java
+++ src/main/java/br/com/blocker/blockersapi/controllers/BlockersController.java
@@ -4,9 +4,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,11 +40,12 @@ public class BlockersController {
     			.body(ipService.save(request)); 
     }
     
-    @GetMapping("/ip")
-    public Mono<Ip> getCustomer(Long id) {
-        return ipService.findById(id);
+    @GetMapping("/{id}")
+    public Mono<Ip> getCustomer(@PathVariable String id) {    	
+    	return ipService.findById(Long.valueOf(id));
     }
-
+    
+    
     @GetMapping("/")
     public Flux<Ip> getAll() {
         return ipService.findAll();
